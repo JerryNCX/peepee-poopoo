01482f9d426b1d441323f8ecfe9bad6fb40f52da
JerryNCX <ncx030715@gmail.com>
2025-01-14 02:38:03 +0800

    FormPage add VehicleInfo, ServiveOpt, D&T, UploadPic&Vid

--- src/FirstPageItem/FormPage.jsx
+++ src/FirstPageItem/FormPage.jsx
@@ -16,6 +16,7 @@ import truck from "../MyImage/SEF Logo.jpg";
 import { InputGroup } from "../components/ui/input-group.jsx";
 import { Field } from "../components/ui/field.jsx";
 import Nationality from "./Nationality.jsx";
+import SelectDate from "./DatePicker.jsx";
 import {
   BrowserRouter,
   Links,
@@ -29,12 +30,17 @@ function FormPage() {
   const navigate = useNavigate();
   return (
     <>
-      <Box className="Page" background="#123456">
+      <Box className="Page" bgColor="#123456">
         <AbsoluteCenter>
           <Flex justifyContent="center">
             <VStack gap="3">
-              <Box fontSize="7xl" paddingBottom="40px">
-                Submit Your Form !
+              <Box>
+                <VStack paddingBottom="40px">
+                  <Text fontSize="7xl" fontWeight="bold">
+                    Spot & Tow
+                  </Text>
+                  <Text fontSize="xl">Car breakdown? Fill this form!!</Text>
+                </VStack>
               </Box>
               <Field label="Name" required>
                 <Input placeholder="Name" className="Input" />
@@ -46,6 +52,19 @@ function FormPage() {
 
               <Field label="Email" required>
                 <Input placeholder="Email" className="Input" />
+              </Field>
+
+              <Field label="Vehicle Information" required>
+                <Input placeholder="Vehicle Information" className="Input" />
+              </Field>
+
+              <Field
+                label="IC / Passport Number"
+                invalid
+                errorText="Only numbers allowed"
+                required
+              >
+                <Input placeholder="IC / Passport Number" className="Input" />
               </Field>
 
               <Field label="Nationality" required>
@@ -60,17 +79,28 @@ function FormPage() {
                 </NativeSelectRoot>
               </Field>
 
-              <Field
-                label="IC / Passport Number"
-                invalid
-                errorText="Only numbers allowed"
-                required
-              >
-                <Input placeholder="IC / Passport Number" background="black" />
+              <Field label="Service Options" required>
+                <NativeSelectRoot
+                  size="sm"
+                  width="-moz-max-content"
+                  variant={"subtle"}
+                >
+                  <NativeSelectField placeholder="Towing">
+                    {/* <ServiceOptions /> */}
+                  </NativeSelectField>
+                </NativeSelectRoot>
+              </Field>
+              
+              <Field label="Select Date & Time">
+                <SelectDate />
+              </Field>
+              
+              <Field label="Upload Mobile Pictures/Videos" >
+                <Input type="file" className="Input" />
               </Field>
 
               <Button
-              _hover={{bg: "green"}}
+                _hover={{ bg: "green" }}
                 colorPalette="peal"
                 onClick={() => navigate("/LoadingPage")}
               >
